8ad2bf2c1cdffd4707f53991aab781cf7deb0f7f
vincentkims49 <vincentkims49@gmail.com>
2023-08-12 14:52:11 +0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import tick from './tick.svg'
+import logo1 from './logo1.png'
+
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    tick,
+    logo1
+}
